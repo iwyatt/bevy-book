486b0c3f958a635233cfcdaaa3073fb7ec3a2a8c
iwyatt <7514713+iwyatt@users.noreply.github.com>
2024-01-28 22:31:48 -0800

    impl 2 second timer

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,14 @@ fn main() {
         .run();
 }
 
-fn hello_world() {
-    println!("hello world!");
-}
-
 #[derive(Component)]
 struct Person;
 
 #[derive(Component)]
 struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
 
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Elaina Proctor".to_string())));
@@ -22,9 +21,11 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Zayna Niecves".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
@@ -41,7 +42,8 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, greet_people));
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+        .add_systems(Startup, add_people)
+        .add_systems(Update, greet_people);
     }
 }
